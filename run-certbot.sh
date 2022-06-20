0d36fcdd101ee51ce12216943fcd11434c6362f6
Phillip Klemm <phil@ptklemm.com>
2022-06-20 05:41:42 +0000

    Create bash script

---
+++ run-certbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Runs the certbot docker image
+sudo docker run -it --rm --name certbot \
+            -v "/etc/letsencrypt:/etc/letsencrypt" \
+            -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+            -p 80:80 \
+            certbot/certbot certonly
